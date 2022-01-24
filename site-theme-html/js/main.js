c37fe5f115127eca164a07fede361a13954b0a21
Saeed Noori <noori.saeed1375@gmail.com>
2022-01-25 00:29:00 +0330

    Complete mega menu and resolve some issue

---
+++ site-theme-html/js/main.js
@@ -0,0 +1,58 @@
+"use strict";
+
+var bg_dark = document.querySelectorAll('.dark-background');
+var menu_item = document.querySelectorAll('.menu-item');
+var mega_menu = document.querySelectorAll('.mega-menu');
+
+menu_item.forEach(function(ele){
+    ele.addEventListener("mouseover",function(){
+        bg_dark.forEach(function(elem){
+            elem.classList.add("active");
+        });
+    });
+    ele.addEventListener("mouseout",function(){
+        bg_dark.forEach(function(elem){
+            elem.classList.remove("active");
+        });
+    });
+});
+
+mega_menu.forEach(function(ele){
+    ele.addEventListener("mouseover",function(){
+        bg_dark.forEach(function(elem){
+            elem.classList.add("active");
+        });
+    });
+    ele.addEventListener("mouseout",function(){
+        bg_dark.forEach(function(elem){
+            elem.classList.remove("active");
+        });
+    });
+});
+
+
+var megaMenus = document.querySelectorAll(".mega-submenu");
+megaMenus.forEach(function (elements) {
+    var mega_menu_target = elements.querySelectorAll('[mega-menu-target]');
+    var mega_menu_id = elements.querySelectorAll('[mega-menu-id]');
+
+    if (!!mega_menu_target) {
+        mega_menu_target.forEach(function (elem) {
+            var megaMenuTarget = elem.getAttribute('mega-menu-target');
+            var megaMenuId = elem.closest('li.mega-menu').querySelector('[mega-menu-id="' + megaMenuTarget + '"]');
+            elem.addEventListener("mouseover", function () {
+                mega_menu_target.forEach(function (ele) {
+                    ele.classList.remove("active");
+                });
+                elem.classList.add("active");
+                mega_menu_id.forEach(function (ele) {
+                    ele.classList.remove("active");
+                });
+
+                if (!!megaMenuId) {
+                    megaMenuId.classList.add("active");
+                }
+            });
+        });
+    }
+}); 
